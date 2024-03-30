8da5ad642a84b392793b4abfc981b8b0d42d80a4
amruta33 <amrutagurav333@gmail.com>
2024-03-30 11:14:53 +0530

    setup addedd

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Project',
+    version= '0.0.0',
+    author= 'Amruta Gurav',
+    author_email= 'amrutagurav333@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
